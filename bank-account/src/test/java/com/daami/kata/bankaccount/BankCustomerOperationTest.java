4017e96527600ff05990e5d91df3326533c1cc62
Montassar DAAMI <montassardaami@gmail.com>
2018-12-03 01:30:41 +0100

    US-1: creation of the unit test with fail compilation

---
+++ bank-account/src/test/java/com/daami/kata/bankaccount/BankCustomerOperationTest.java
@@ -0,0 +1,11 @@
+package com.daami.kata.bankaccount;
+
+public class BankCustomerOperationTest {
+
+	public void customer_should_do_deposit_operation() {
+
+		BankCustomerOperationServiceImpl bankCustomerOperationServiceImpl = new BankClientOperationServiceImpl();
+
+	}
+
+}
