7a716e3c7dc39a71d147fe17dff806fc2535fda3
Montassar DAAMI <montassardaami@gmail.com>
2018-12-03 02:28:05 +0100

    US-1: code refactoring

--- bank-account/src/main/java/com/daami/kata/bankaccount/service/BankCustomerOperationServiceImpl.java
+++ bank-account/src/main/java/com/daami/kata/bankaccount/service/BankCustomerOperationServiceImpl.java
@@ -1,11 +1,13 @@
 package com.daami.kata.bankaccount.service;
 
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.daami.kata.bankaccount.exception.ServiceException;
 import com.daami.kata.bankaccount.model.Account;
@@ -15,29 +17,42 @@ import com.daami.kata.bankaccount.model.OperationType;
 
 public class BankCustomerOperationServiceImpl {
 
-    LocalDateTime transactionDateTime = LocalDate.of( 2018, 10, 1 )
-                                                 .atStartOfDay();
+	LocalDateTime transactionDateTime = LocalDate.of(2018, 10, 1).atStartOfDay();
 
-    public Customer deposit( final Customer customer, final BigDecimal depositValue ) {
-        return Optional.ofNullable( customer )
-                       .map( customerO -> Customer.builder()
-                                                  .name( customerO.getName() )
-                                                  .id( customerO.getId() )
-                                                  .account( Account.builder()
-                                                                   .accountId( customerO.getAccount()
-                                                                                        .getAccountId() )
-                                                                   .accountBalance( customerO.getAccount()
-                                                                                             .getAccountBalance()
-                                                                                             .add( depositValue ) )
-                                                                   .accountTransactions( Arrays.asList( AccountTransaction.builder()
-                                                                                                                          .amount( depositValue )
-                                                                                                                          .operationType(
-                                                                                                                                  OperationType.DEPOSIT_OPERATION )
-                                                                                                                          .transactionDate(
-                                                                                                                                  transactionDateTime )
-                                                                                                                          .build() ) )
-                                                                   .build() )
-                                                  .build() )
-                       .orElseThrow( () -> new ServiceException( "" ) );
-    }
+	public Customer deposit(final Customer customer, final BigDecimal depositValue) {
+		return Optional.ofNullable(customer)
+					.map(customerO -> createNewCustomer(customerO, depositValue))
+					.orElseThrow(() -> new ServiceException(""));
+	}
+
+	private Customer createNewCustomer(final Customer actualCustomer, final BigDecimal depositValue) {
+		return Customer.builder()
+					.name(actualCustomer.getName())
+					.id(actualCustomer.getId())
+					.account(createNewClientAccount(depositValue, actualCustomer.getAccount())).build();
+	}
+
+	private Account createNewClientAccount(final BigDecimal depositValue, final Account account) {
+		return Account.builder()
+					.accountId(account.getAccountId())
+					.accountBalance(account.getAccountBalance().add(depositValue))
+					.accountTransactions(createNewListOfTransaction(account.getAccountTransactions(), depositValue))
+				.	build();
+	}
+
+	private List<AccountTransaction> createNewListOfTransaction(List<AccountTransaction> accountTransactions,
+			final BigDecimal depositValueOfNewTransaction) {
+
+		return Stream
+				.concat(accountTransactions.stream(),
+						Arrays.asList(createNewDepositTransaction(depositValueOfNewTransaction)).stream())
+				.distinct().collect(Collectors.toList());
+	}
+
+	private AccountTransaction createNewDepositTransaction(final BigDecimal depositValue) {
+		return AccountTransaction.builder()
+								.amount(depositValue)
+								.operationType(OperationType.DEPOSIT_OPERATION)
+								.transactionDate(transactionDateTime).build();
+	}
 }
