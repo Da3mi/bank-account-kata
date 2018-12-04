5b03a241c424b19c5521429f4bb36798c476989d
Montassar DAAMI <montassar.daami@talan.com>
2018-12-04 15:55:40 +0100

    US-3: unit test refactoring, unit test runs

--- bank-account/src/test/java/com/daami/kata/bankaccount/BankCustomerOperationTest.java
+++ bank-account/src/test/java/com/daami/kata/bankaccount/BankCustomerOperationTest.java
@@ -20,14 +20,16 @@ import com.daami.kata.bankaccount.service.impl.BankCustomerOperationServiceImpl;
 @SpringBootTest
 public class BankCustomerOperationTest {
 
-	private final static String ACCOUNT_HEADER_TEMPLATE = "%s \n Today: %s \n \n"
-			+ "+-----------------------------------------------------------+\n"
-			+ "		Date		|  		Operations		| 		Balance		\n"
-			+ "+-----------------------------------------------------------+\n"
-			+ "											  %s				\n";
+	private final static String ACCOUNT_HEADER_TEMPLATE = "%s \n"
+			+ "Today: %s \n"
+			+ "+--------------------------------------------------------------------+\n"
+			+ "		Date	|	Operations	|	Balance \n"
+			+ "+--------------------------------------------------------------------+\n"
+			+ " %s	|			| %s \n";
+			
 	
 	private final static String ACCOUNT_OPERATION_LINE_TEMPLATE = ""
-			+ " %s				| %s 					| %s		\n";
+			+ " %s	| %s			| %s \n";
 	
 	
 	@Test
@@ -125,7 +127,7 @@ public class BankCustomerOperationTest {
 		AccountTransaction depositTransaction = AccountTransaction.builder()
 				.transactionDate(transactionDateTime)
 				.amount(depositValue)
-				.operationType(OperationType.WITHDRAWAL_OPERATION).build();
+				.operationType(OperationType.DEPOSIT_OPERATION).build();
 		
 		AccountTransaction withdrawTransaction0 = AccountTransaction.builder()
 				.transactionDate(transactionDateTime)
@@ -140,7 +142,7 @@ public class BankCustomerOperationTest {
 		Account account = Account.builder()
 									.accountId("1l")
 									.accountTransactions(Arrays.asList(depositTransaction, withdrawTransaction0, withdrawTransaction1))
-									.accountBalance(depositValue.subtract(withdrawValue).subtract(withdrawValue))
+									.accountBalance(BigDecimal.valueOf( 100 ))
 									.build();
 		
 		Customer customer = Customer.builder()
@@ -152,28 +154,41 @@ public class BankCustomerOperationTest {
 		String customerBankAccountHistory = bankAccountStatementServiceImpl.showCsutomerAccountHistory(customer);
 		
 		
+		Account expectedAccount = Account.builder()
+				.accountId("1l")
+				.accountTransactions(Arrays.asList(depositTransaction, withdrawTransaction0, withdrawTransaction1))
+				.accountBalance(BigDecimal.valueOf( 100 ))
+				.build();
 		
-		String expectedAccounthistoryheader = String.format(ACCOUNT_HEADER_TEMPLATE , customer.getName(), LocalDate.now(), customer.getAccount().getAccountBalance());
+		
+		Customer expectedCustomer = Customer.builder()
+				.account(expectedAccount)
+				.name("Montassar")
+				.id(new Long(1))
+				.build();
+		
+		
+		String expectedAccountHistoryheader = String.format(ACCOUNT_HEADER_TEMPLATE , expectedCustomer.getName(), LocalDate.now(), LocalDate.now().atStartOfDay(), expectedCustomer.getAccount().getAccountBalance());
 		
 		String operation0 = String.format(ACCOUNT_OPERATION_LINE_TEMPLATE , depositTransaction.getTransactionDate(), 
 				depositTransaction.getOperationType().getOperation().concat(depositTransaction.getAmount().toString()),
-				depositValue);
+				expectedCustomer.getAccount().getAccountBalance().add(depositValue));
 		
 		String operation1 = String.format(ACCOUNT_OPERATION_LINE_TEMPLATE , withdrawTransaction0.getTransactionDate(), 
 				withdrawTransaction0.getOperationType().getOperation().concat(withdrawTransaction0.getAmount().toString()),
-				depositValue.subtract(withdrawValue));
+				expectedCustomer.getAccount().getAccountBalance().add(depositValue).subtract(withdrawValue));
 		
 		String operation2 = String.format(ACCOUNT_OPERATION_LINE_TEMPLATE , withdrawTransaction1.getTransactionDate(), 
 				withdrawTransaction1.getOperationType().getOperation().concat(withdrawTransaction1.getAmount().toString()),
-				depositValue.subtract(withdrawValue).subtract(withdrawValue));
+				expectedCustomer.getAccount().getAccountBalance().add(depositValue).subtract(withdrawValue).subtract(withdrawValue));
 		
-		StringBuilder expectedAccounthistory = new StringBuilder(expectedAccounthistoryheader)
+		StringBuilder expectedAccountHistory = new StringBuilder(expectedAccountHistoryheader)
 				.append(operation0).append(operation1).append(operation2);
 		
-		System.out.println(expectedAccounthistory.toString());
+		System.out.println(expectedAccountHistory.toString());
 		
-		assertThat(customerBankAccountHistory).isNotBlank();
-		assertThat(expectedAccounthistory.toString()).isEqualTo(customerBankAccountHistory);
+		assertThat(customerBankAccountHistory).isNotEmpty();
+		assertThat(expectedAccountHistory.toString()).isEqualTo(customerBankAccountHistory);
 				
 	}
 	
