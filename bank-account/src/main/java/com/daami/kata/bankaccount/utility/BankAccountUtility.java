60b0de31c7c830375d70cdfeb757763b9a3d030a
Montassar DAAMI <montassar.daami@talan.com>
2018-12-06 11:30:16 +0100

    add constraint to manage negative balance

--- bank-account/src/main/java/com/daami/kata/bankaccount/utility/BankAccountUtility.java
+++ bank-account/src/main/java/com/daami/kata/bankaccount/utility/BankAccountUtility.java
@@ -9,9 +9,15 @@ import com.daami.kata.bankaccount.model.OperationType;
 public final class BankAccountUtility {
 
 	public static BigDecimal computeBalance(final BigDecimal actualBalance, final BigDecimal transactionAmount, final OperationType operationType ) {
-		return Optional.ofNullable(actualBalance).map( balance -> 
+		BigDecimal accountBalance = Optional.ofNullable(actualBalance).map( balance -> 
 				OperationType.DEPOSIT_OPERATION.equals(operationType) ? balance.add(transactionAmount) : balance.subtract(transactionAmount))
 				.orElseThrow(() -> new ServiceException("internal technical error"));
+		
+		if (accountBalance.compareTo(BigDecimal.ZERO) < 0){
+			throw new ServiceException("balance is insufficient to do withdraw operation");
+		}
+		
+		return accountBalance;
 	}
 	
 	
